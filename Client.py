65f150477e6f8c5509a88511bb7a9d2e7de5a751
Abhinav Palacharla <abhinav.palacharla@gmail.com>
2020-02-21 21:52:08 -0800

    fixed client recieving encoded bytes, decoded them, and various other issues

--- Client.py
+++ Client.py
@@ -4,32 +4,37 @@ import shutil
 
 class Client:
 
+	#cfile = None
+
 	def __init__(self):
+		self.cfile = None
+		global s
 		s = socket.socket()
-		port = 1942
+		port = 908
 		host = self.query_hostname()
 
 		print(f"[+] Connecting to {host} on port {port}")
 		s.connect((host,port))
 
-		cfile = self.query_download()
+		self.cfile = self.query_download()
+		self.download_file()
 
 	def query_hostname(self):
 		hostname = input(str("[+] Enter the hostname of the server:  "))
 		return hostname
 
 	def query_download(self):
-		path = input(str("[+] Input the full path of the folder where you would like downloads to go: ")) #holds path to files
-		return path
+		file = input(str("[+] Input the name you would like the file to be saved to: ")) #holds path to files
+		return file
 
 	def download_file(self):
 		cwd = os.getcwd()
-		shutil.rmtree(f"{cwd}/downloads")#clear downloads folder before usage
-        f = open(cfile, "w+")
-		file_data = f.recv(1024) #get data from server peer
-		f.write(file_data) #write data to user dwfined file
+		f = open(f"{cwd}/downloads/{self.cfile}", "w+")
+		file_data = s.recv(1024) #get data from server peer
+		decoded = file_data.decode("utf-8") #need to decode bytes that were sent by server
+		f.write(decoded)
 		f.close()
 
 
-
-client = Client()
+if __name__ == "__main__":
+	client = Client()
--- Server.py
+++ Server.py
@@ -1,33 +1,37 @@
 import socket
 import os
+import shutil
 
 class Server:
 
+	sfile = None
+
+
 	def __init__(self):
-		self.file = None
 		global conn, addr
 
 		s = socket.socket()
 		host = socket.gethostname()
 		print(f"host: {host}")
-		port = 1942
+		port = 908
 		s.bind((host,port))
 		s.listen(1) #only one connection
 
 		conn, addr = s.accept()
-		print('[+] A client has connected')
-		print(conn)
+		print('[+] Client has connected')
 
-		self.get_file()
+		self.sfile = self.get_file()
 		self.send_file()
 
 	def get_file(self):
-		self.file = input(str("[+] Input the full path of the file you would like to send: ")) #holds path to files
+		file = input(str("[+] Input the name of the file you would like to send: ")) #holds path to files
+		return file
 
 	def send_file(self):
-		f = open(self.file, 'rb')
+		cwd = os.getcwd()
+		f = open(f"{cwd}/uploads/{self.sfile}", 'rb')
 		file_data = f.read(1024)
-		conn.send(file_data)
+		conn.send(file_data) #file sent in utf-8 bytes
 
 if __name__ == "__main__":
     server = Server()
